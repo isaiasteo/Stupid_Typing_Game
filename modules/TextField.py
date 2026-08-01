61b0da622e527551148c1ab6b309fa0ccdf8a42e
isaiasteo <xcvrty137869585ggjhdk@gmail.com>
2026-08-01 15:32:12 -0300

    Made it less ugly. Added styles to protect against eye strain. Modified: TextField.py

--- modules/TextField.py
+++ modules/TextField.py
@@ -8,6 +8,17 @@ class TextField(wx.Panel):
         sizer = wx.BoxSizer(wx.VERTICAL)
 
         self.text = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.text.SetBackgroundColour(wx.Colour(20, 20, 20))
+        font = wx.Font(
+            14,
+            wx.FONTFAMILY_DEFAULT,
+            wx.FONTSTYLE_NORMAL,
+            wx.FONTWEIGHT_NORMAL,
+            faceName="Segoe UI",
+        )
+        self.text.SetForegroundColour(wx.Colour(255, 255, 255))  # 0 - 255
+        self.text.SetFont(font)
+
         sizer.Add(self.text, 1, wx.ALL | wx.EXPAND, 10)
 
         self.SetSizer(sizer)
