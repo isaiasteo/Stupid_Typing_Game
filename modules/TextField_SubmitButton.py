b353c1f9c01c3aea9722b01a7c39d5d91bbbdaa0
isaiasteo <xcvrty137869585ggjhdk@gmail.com>
2026-08-03 14:23:22 -0300

    Submit button also changes the window.

--- modules/TextField_SubmitButton.py
+++ modules/TextField_SubmitButton.py
@@ -6,11 +6,13 @@ from modules.StoreText import StoreText
 def create_submit_button(panel, text_field):
     button = wx.Button(panel, label="Submit")
 
-    button.Bind(wx.EVT_BUTTON, lambda event: on_submit(event, text_field))
+    button.Bind(wx.EVT_BUTTON, lambda event: on_submit(event, text_field, panel))
 
     return button
 
 
-def on_submit(event, text_field):
+def on_submit(event, text_field, panel):
     user_text = text_field.GetValue()
     StoreText(user_text)
+
+    panel.Destroy()
