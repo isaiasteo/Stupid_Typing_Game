09a7e753e14593514975d6aa09df3538b8c955eb
isaiasteo <xcvrty137869585ggjhdk@gmail.com>
2026-08-04 09:19:28 -0300

    Styled displaytext.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ panel = TextField(window)
 sizer = wx.BoxSizer(wx.VERTICAL)
 sizer.Add(panel, 1, wx.EXPAND)
 window.SetSizer(sizer)
+window.SetBackgroundColour(wx.BLACK)
 
 window.Show()
 
--- modules/DisplayText.py
+++ modules/DisplayText.py
@@ -1,11 +1,17 @@
 import wx
-from .TextField_style import apply_style
 
 
 def display_text(panel, text):
     label = wx.StaticText(panel, label=text)
 
-    panel.GetSizer().Add(label, 0, wx.ALL | wx.CENTER, 20)
+    font = label.GetFont()
+    font.SetPointSize(14)
+    label.SetBackgroundColour(wx.Colour(20, 20, 20))
+    label.SetForegroundColour(wx.Colour(255, 255, 255))
+    faceName = "Segoe UI"
+    label.SetFont(font)
+
+    panel.GetSizer().Add(label, 1, wx.ALL | wx.CENTER, 20)
 
     panel.Layout()
 
