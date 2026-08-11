7a7813a85e39a4553b73dc83125ed3f3e3cd4b17
isaiasteo <xcvrty137869585ggjhdk@gmail.com>
2026-08-11 12:23:12 -0300

    harmless console error removed

--- modules/display_text/text_glitch.py
+++ modules/display_text/text_glitch.py
@@ -13,17 +13,22 @@ class text_glitch(wx.Panel):
 
         self.Bind(wx.EVT_TIMER, self._update_glitch)
         self.Bind(wx.EVT_PAINT, self._on_paint)
+        self.Bind(wx.EVT_WINDOW_DESTROY, self._on_destroy)
 
         self.duration = 500
         self.frame_interval = 40
-
         self.elapsed = 0
 
         self.Hide()
 
     def start(self):
+        if self.IsBeingDeleted():
+            return
 
         parent = self.GetParent()
+
+        if not parent or parent.IsBeingDeleted():
+            return
 
         label = parent.label
 
@@ -34,31 +39,38 @@ class text_glitch(wx.Panel):
 
         self.Show()
         self.Raise()
-
         self.Refresh()
 
-        self.timer.Start(self.frame_interval)
+        if not self.timer.IsRunning():
+            self.timer.Start(self.frame_interval)
 
     def _update_glitch(self, event):
+        if self.IsBeingDeleted():
+            return
 
         self.elapsed += self.frame_interval
 
         if self.elapsed >= self.duration:
-
             self.timer.Stop()
-
             self.Hide()
 
-            self.GetParent().label.Refresh()
-            self.GetParent().label.Update()
+            parent = self.GetParent()
+
+            if parent and not parent.IsBeingDeleted():
+                parent.label.Refresh()
 
             return
 
         self.Refresh()
 
-    def _on_paint(self, event):
+    def _on_destroy(self, event):
+        if self.timer.IsRunning():
+            self.timer.Stop()
 
-        dc = wx.AutoBufferedPaintDC(self)
+        event.Skip()
+
+    def _on_paint(self, event):
+        dc = wx.PaintDC(self)
 
         width, height = self.GetClientSize()
 
@@ -66,7 +78,6 @@ class text_glitch(wx.Panel):
         dc.Clear()
 
         for _ in range(250):
-
             x = random.randrange(max(1, width))
             y = random.randrange(max(1, height))
 
@@ -77,7 +88,6 @@ class text_glitch(wx.Panel):
             colour = wx.Colour(brightness, brightness, brightness)
 
             dc.SetBrush(wx.Brush(colour))
-
             dc.DrawRectangle(x, y, size, size)
 
         dc.SetPen(wx.Pen(wx.Colour(80, 80, 80), 1))
@@ -86,13 +96,10 @@ class text_glitch(wx.Panel):
             dc.DrawLine(0, y, width, y)
 
         for _ in range(12):
-
             y = random.randrange(max(1, height))
 
             bar_height = random.randint(1, 6)
-
             x = random.randint(-50, 50)
-
             bar_width = random.randint(20, max(21, width))
 
             brightness = random.randint(80, 255)
@@ -100,5 +107,4 @@ class text_glitch(wx.Panel):
             colour = wx.Colour(brightness, brightness, brightness)
 
             dc.SetBrush(wx.Brush(colour))
-
             dc.DrawRectangle(x, y, bar_width, bar_height)
