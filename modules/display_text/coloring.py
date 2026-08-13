c64418a22067a9d7fb5bbd2b58f0e034e80113dc
isaiasteo <xcvrty137869585ggjhdk@gmail.com>
2026-08-13 12:08:25 -0300

    bug fix on dyeing permanently

--- modules/display_text/coloring.py
+++ modules/display_text/coloring.py
@@ -3,11 +3,28 @@ import wx.richtext
 
 
 def paint_progress(text_control, position):
+    reset_color(text_control, text_control.GetValue().__len__())
+
     style = wx.TextAttr()
     style.SetTextColour(wx.Colour(255, 105, 180))
 
+    text = text_control.GetValue()
+
+    display_position = 0
+    logical_position = 0
+
+    for character in text:
+
+        if logical_position >= position:
+            break
+
+        display_position += 1
+
+        if character != "⏎":
+            logical_position += 1
+
     text_control.SetStyle(
-        wx.richtext.RichTextRange(0, position),
+        wx.richtext.RichTextRange(0, display_position),
         style,
     )
 
