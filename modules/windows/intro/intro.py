66745fa510746bf32c623e97b792c0dc93224a8f
isaiasteo <xcvrty137869585ggjhdk@gmail.com>
2026-09-09 02:41:35 -0300

    Pixel into

--- modules/windows/intro/intro.py
+++ modules/windows/intro/intro.py
@@ -1,6 +1,7 @@
 import wx
 
 from modules.sfx.sound_effects import sfx
+from modules.windows.intro.pixel import PixelPanel
 
 
 class intro(wx.Panel):
@@ -12,24 +13,10 @@ class intro(wx.Panel):
         self.data = data
         self.next_screen = next_screen
 
-        self.result_text = wx.StaticText(
-            self,
-            label="github.com/isaiasteo",
-        )
-
-        self.result_text.SetForegroundColour(wx.WHITE)
-
-        font = self.result_text.GetFont()
-        font.SetPointSize(30)
-        self.result_text.SetFont(font)
+        self.pixel_panel = PixelPanel(self)
 
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(
-            self.result_text,
-            0,
-            wx.ALIGN_CENTER | wx.TOP,
-            500,
-        )
+        sizer.Add(self.pixel_panel, 1, wx.EXPAND)
 
         self.SetSizer(sizer)
 
